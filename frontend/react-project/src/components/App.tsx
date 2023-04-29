c5278175341b506fe36636595947d510e97c584e
Tom220413 <t-matsuzaki@zhienius.co.jp>
2023-04-29 18:00:10 +0900

    topページは完成、次は検索コンポーネントとかを作る

--- frontend/react-project/src/components/App.tsx
+++ frontend/react-project/src/components/App.tsx
@@ -1,15 +1,9 @@
 import React, { useRef, useState } from "react";
-import { BrowserRouter as Router, Redirect, Route, RouteProps, Switch, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, RouteProps, Switch } from 'react-router-dom';
 
-import { useTodo } from "./useTodo";
-import { Todo } from "../types/Todo";
-import { TodoAdd } from "./TodoAdd";
-import { TodoList } from "./TodoList";
-import { TodoTitle } from "./TodoTitle";
 import { Map } from "./Map";
 import SearchHeader from './SearchHeader';
 import "./styles.css";
-import ReviewSlider from './ReviewSlider';
 import { Search } from "./Search";
 import { Tab, Review, AuthUser } from "../types/types";
 import { AuthUserProvider, useAuthUser } from './AuthUserContext';
@@ -20,176 +14,85 @@ import ProfilePage from './ProfilePage';
 
 
 function App() {
-    const { todoList, toggleTodoListItemStatus, addTodoListItem, deleteTodoListItem } = useTodo();
-    const inputEl = useRef<HTMLTextAreaElement>(null);
     const [activeTab, setActiveTab] = useState(0);
-    const handleAddTodoListItem = () => {
-        if (inputEl.current?.value === "") {
-            return;
-        }
-        addTodoListItem(inputEl.current!.value);
-        inputEl.current!.value = "";
-    };
-    const incompletedList = todoList.filter((todo: Todo) => !todo.done);
-    const completedList = todoList.filter((todo: Todo) => todo.done);
-    const renderTodoList = (title: string, list: Todo[]) => {
-        return (
-            <TodoList
-                todoList={list}
-                toggleTodoListItemStatus={toggleTodoListItemStatus}
-                deleteTodoListItem={deleteTodoListItem}
-                title={title}
-                as="h2"
-            />
-        );
-    };
-    const reviews: Review[] = [
-        {
-            id: 1,
-            author: 'John Smith',
-            text: 'えええ'
-        },
-        {
-            id: 2,
-            author: 'Jane Doe',
-            text: 'ううう'
-        },
-        {
-            id: 3,
-            author: 'Mike Johnson',
-            text: 'いいい'
-        },
-        {
-            id: 4,
-            author: 'Alice Williams',
-            text:
-                'あああ',
-        },
-        {
-            id: 5,
-            author: 'Alice Williams',
-            text:
-                'おおお',
-        },
 
-    ]
-    // テスト的に値を入れている　本来はdbから値を取ってくる
-    const rankings: Review[] = [
-        {
-            id: 1,
-            author: '1位',
-            text: 'えええ'
-        },
-        {
-            id: 2,
-            author: '2位',
-            text: 'ううう'
-        },
-        {
-            id: 3,
-            author: '3位',
-            text: 'いいい'
-        },
-        {
-            id: 4,
-            author: '4位',
-            text:
-                'あああ',
-        },
-        {
-            id: 5,
-            author: '5位',
-            text:
-                'おおお',
-        },
-
-    ]
-    const serch = () => {
+    const top = () => {
         return (
-            <Search />
-        )
-    }
-    const ranking = (rankings: Review[]) => {
-        return (
-            <ReviewSlider reviews={rankings || []} />
-        )
-    }
-    const review = (reviews: Review[]) => {
-        return (
-            <ReviewSlider reviews={reviews || []} />
+            < HomePage />
         )
     }
     const tabs: Tab[] = [
         {
             id: 0,
             label: "トップ",
-            content: renderTodoList("未完了TODOリスト", incompletedList),
+            content: top(),
         },
         {
             id: 1,
             label: "検索",
-            content: serch(),
+            content: top(),
         },
         {
             id: 2,
             label: "ランキング",
-            content: ranking(rankings),
+            content: top(),
         },
         {
             id: 3,
             label: "レビュー",
-            content: review(reviews),
+            content: top(),
         },
         {
             id: 4,
             label: "マイページ",
-            content: renderTodoList("完了TODOリスト", completedList),
+            content: top(),
         },
     ];
-    const UnAuthRoute: React.FC<RouteProps> = ({ ...props }) => {
-        const authUser = useAuthUser()
-        const isAuthenticated = authUser != null
-        const { from = "/" } = useLocation<{ from: string }>().state ?? {}
+    // const UnAuthRoute: React.FC<RouteProps> = ({ ...props }) => {
+    //     const authUser = useAuthUser()
+    //     const isAuthenticated = authUser != null
+    //     const { from = "/" } = useLocation<{ from: string }>().state ?? {}
 
-        if (isAuthenticated) {
-            console.log(`ログイン済みのユーザーは${props.path}へはアクセスできません`)
-            return <Redirect to={from} />
-        } else {
-            return <Route {...props} />
-        }
-    }
+    //     if (isAuthenticated) {
+    //         console.log(`ログイン済みのユーザーは${props.path}へはアクセスできません`)
+    //         return <Redirect to={from} />
+    //     } else {
+    //         return <Route {...props} />
+    //     }
+    // }
 
-    const PrivateRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
-        const authUser = useAuthUser();
-        const isAuthenticated = authUser != null;
+    // const PrivateRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
+    //     const authUser = useAuthUser();
+    //     const isAuthenticated = authUser != null;
 
-        if (isAuthenticated) {
-            return <Route {...rest}>{children}</Route>;
-        } else {
-            console.log(`ログインしていないユーザーは${rest.path}へはアクセスできません`);
-            return <Redirect to="/login" />;
-        }
-    };
+    //     if (isAuthenticated) {
+    //         return <Route {...rest}>{children}</Route>;
+    //     } else {
+    //         console.log(`ログインしていないユーザーは${rest.path}へはアクセスできません`);
+    //         return <Redirect to="/login" />;
+    //     }
+    // };
 
     return (
         <>
-            <SearchHeader />
-            <nav className="tabs">
-                {tabs.map((tab) => (
-                    <a
-                        key={tab.id}
-                        className={activeTab === tab.id ? "active" : ""}
-                        onClick={() => setActiveTab(tab.id)}
-                    >
-                        {tab.label}
-                    </a>
-                ))}
-            </nav>
-            <div className="tab-content">{tabs[activeTab].content}</div>
-            <TodoTitle title="レビューを追加" as="h1" />
-            <TodoAdd buttonText="+ 投稿" inputEl={inputEl} handleAddTodoListItem={handleAddTodoListItem} />
-            {/* <Map /> */}
-            <AuthUserProvider>
+            <div className="maincontainer">
+                <SearchHeader />
+                <nav className="tabs">
+                    {tabs.map((tab) => (
+                        <a
+                            key={tab.id}
+                            className={activeTab === tab.id ? "active" : ""}
+                            onClick={() => setActiveTab(tab.id)}
+                        >
+                            {tab.label}
+                        </a>
+                    ))}
+                </nav>
+                <div className="tab-content">{tabs[activeTab].content}</div>
+                {/* <TodoTitle title="レビューを追加" as="h1" /> */}
+                {/* <TodoAdd buttonText="+ 投稿" inputEl={inputEl} handleAddTodoListItem={handleAddTodoListItem} /> */}
+                {/* <Map /> */}
+                {/* <AuthUserProvider>
                 <Router>
                     <Switch>
                         <UnAuthRoute exact path="/login" component={LoginPage} />
@@ -199,7 +102,8 @@ function App() {
                         <Redirect to="/" />
                     </Switch>
                 </Router>
-            </AuthUserProvider>
+            </AuthUserProvider> */}
+            </div>
         </>
     );
 }
--- frontend/react-project/src/components/HomePage.tsx
+++ frontend/react-project/src/components/HomePage.tsx
@@ -1,21 +1,107 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
-import { useLogout } from './AuthUserContext'
+import ReviewSlider from './ReviewSlider';
+import { Review } from "../types/types";
 
 const HomePage = () => {
-    const history = useHistory()
-    const logout = useLogout()
+    const reviews: Review[] = [
+        {
+            id: 1,
+            author: 'John Smith',
+            text: 'えええ'
+        },
+        {
+            id: 2,
+            author: 'Jane Doe',
+            text: 'ううう'
+        },
+        {
+            id: 3,
+            author: 'Mike Johnson',
+            text: 'いいい'
+        },
+        {
+            id: 4,
+            author: 'Alice Williams',
+            text:
+                'あああ',
+        },
+        {
+            id: 5,
+            author: 'Alice Williams',
+            text:
+                'おおお',
+        },
+
+    ]
+    // テスト的に値を入れている　本来はdbから値を取ってくる
+    const rankings: Review[] = [
+        {
+            id: 1,
+            author: '1位',
+            text: 'えええ'
+        },
+        {
+            id: 2,
+            author: '2位',
+            text: 'ううう'
+        },
+        {
+            id: 3,
+            author: '3位',
+            text: 'いいい'
+        },
+        {
+            id: 4,
+            author: '4位',
+            text:
+                'あああ',
+        },
+        {
+            id: 5,
+            author: '5位',
+            text:
+                'おおお',
+        },
+
+    ]
+
+    const ranking = (rankings: Review[]) => {
+        return (
+            <ReviewSlider reviews={rankings || []} />
+        )
+    }
+    const review = (reviews: Review[]) => {
+        return (
+            <ReviewSlider reviews={reviews || []} />
+        )
+    }
+
     return (
         <>
-            <h1>ホーム</h1>
-            <p>プライベートページです。ログインしていないユーザは見れません。</p>
-            <div>
-                <button onClick={logout}>ログアウトする</button>
-                <button onClick={() => history.push("/login")}>/loginへアクセスしてみる</button>
-                <button onClick={() => history.push("/logout")}>/logoutへアクセスしてみる</button>
-                <button onClick={() => history.push("/profile/1")}>ユーザー1のプロフィールページへ</button>
-                <button onClick={() => history.push("/profile/2")}>ユーザー2のプロフィールページへ</button>
-                <button onClick={() => history.push("/profile/3")}>ユーザー3のプロフィールページへ</button>
+            <div className='contens-box'>
+                <div className='titlebox'>
+                    <table className='titletable'>
+                        <tr>
+                            <td>
+                                <h2 className='homepagetitle'>もつ鍋探しは
+                                    <br></br>
+                                    こちらへ▷▶︎▷▷▶︎▷</h2 >
+                            </td>
+                            <td>
+                                <a href="https://example.com">
+                                    <img className="motuimg" src="img/food_motsunabe.png" /><br />
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div className='reviewbox'>
+                    {review(reviews)}
+                </div>
+                <br />
+                <div className='rankingbox'>
+                    {ranking(rankings)}
+                </div>
             </div>
         </>
     )
